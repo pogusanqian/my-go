e407aa13bfe958325ba3ef55366b99893bb9051c
pogusanqian <pogusanqian@163.com>
2022-01-26 14:03:16 +0800

    int类型

--- src/go_code/project01/main/02-变量.go
+++ src/go_code/project01/main/02-变量.go
@@ -1,6 +1,12 @@
 package main
 import "fmt"
 
+/**
+	变量注意事项:
+		1. 如果变量声明了类型, 可以改变值, 但是不能改变数据类型(JS是弱类型语言, 既能改变值, 又能改变数据类型)
+		2. 声明变量后, 如果没有赋值, go会自动添加默认值(JS中是undefined)
+		3. GO使用var声明的变量是块级作用域(JS中使用var声明的是函数级作用域)
+*/
 // 声明全局变量
 var county = "中国"
 
@@ -9,7 +15,7 @@ func main() {
 	var age1 int
 	fmt.Println("age1:", age1)
 
-	// 2.根据值自动判定类型
+	// 2.根据值自动判定类型, 也可以写成var age2 int = 25;
 	var age2 = 25
 	fmt.Println("age2:", age2)
 
---
+++ src/go_code/project01/main/03-整数数据类型.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"unsafe"
+) 
+
+/**
+	整数数据类型
+		1. int: 有符号, 32位系统中占4个字节, 64位系统中占用8个字节, 所以int和int32是不同的数据类型
+		2. uint: 无符号的int类型
+		3. rune: 等价于int32
+		4. byte: 1个字节空间, 存储数据类型时, 相当于时int8
+		5. go语言中, var num = 100; 其中num的数据类型默认就是int数据类型
+		6. 声明变量原则: 再程序正常行运行时, 尽量采用占用空间小的数据类型
+*/
+func main() {
+	// 查看变量的数据类型
+	fmt.Printf("%T", 100)
+	fmt.Println()
+	// 查看变量的内存大小
+	fmt.Printf("%d", unsafe.Sizeof(100))
+}
